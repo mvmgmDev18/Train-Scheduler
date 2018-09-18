1a6ac94c79b8633f50c3004dbc76089358311ec6
Michael S <user@mail.com>
2018-09-18 01:09:38 -0700

    firebase added

---
+++ assets/javascript/main.js
@@ -0,0 +1,13 @@
+<script src="https://www.gstatic.com/firebasejs/5.5.0/firebase.js"></script>
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyA95pen7bK1bxkCaxGAZqxIYaXD6RFPU80",
+    authDomain: "train-scheduler-58006.firebaseapp.com",
+    databaseURL: "https://train-scheduler-58006.firebaseio.com",
+    projectId: "train-scheduler-58006",
+    storageBucket: "",
+    messagingSenderId: "534103018332"
+  };
+  firebase.initializeApp(config);
+</script>
